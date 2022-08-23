4f937352e1ae94ceca5512fc598e61a710031f25
Dmitry <kochetov.dmitry@mail.ru>
2022-08-23 14:45:42 +0300

    добавлены решения всех задач

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+Console.Clear();
+
+Console.Write("Задайте M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+Console.Write($"M = {m}; N = {n} - > {NaturalNumberSum(m, n, 0)}");
+
+
+int NaturalNumberSum(int from, int to, int sum)
+{
+    
+    sum = sum + from;
+    from++;
+    if (from > to) return sum;
+    return NaturalNumberSum(from, to, sum);
+}
--- Task68/Program.cs
+++ Task68/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана 
+с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+m = 3, n = 2 -> A(m,n) = 29
+ */
+
+
+Console.Clear();
+
+Console.Write("Задайте M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"M = {m}; N = {n} - > A(m,n) = {AF(m, n)}");
+
+int AF(int m, int n) 
+{
+    if (m == 0) return n + 1;
+    if (n == 0) return AF(m - 1, 1);
+    return AF(m - 1, AF(m, n - 1));
+}
